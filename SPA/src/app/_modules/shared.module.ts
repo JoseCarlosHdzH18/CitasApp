8dc74bee943d539550ae55601a29cce613d168e1
Jose Carlos Hernandez <jose.carlos@lionintel.com>
2024-02-22 00:54:07 -0600

    feat: :sparkles: Added Loading Indicator/service

--- SPA/src/app/_modules/shared.module.ts
+++ SPA/src/app/_modules/shared.module.ts
@@ -1,26 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ToastrModule } from 'ngx-toastr';
+import { timeout } from 'rxjs';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
+    FormsModule,
+    BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       timeOut: 10000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: false,
-      progressBar: true
+      closeButton: true
     }),
+    NgxSpinnerModule.forRoot({
+      type: 'ball-scale-multiple'
+    })
   ],
-  exports: [
+  exports:
+  [
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }

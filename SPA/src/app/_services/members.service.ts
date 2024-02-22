e35a7ecaf2f1b869358ca4c9a13ab7820f01de7b
Jose Carlos Hernandez <jose.carlos@lionintel.com>
2024-02-22 06:53:55 -0600

    feat: :sparkles: Cloudinary Service

--- SPA/src/app/_services/members.service.ts
+++ SPA/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IMember } from '../_models/imember';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'

2f1971b89eff835c6e8981748278b439f200c525
Jose Carlos Hernandez <jose.carlos@lionintel.com>
2024-03-17 19:41:42 -0600

    feat: :sparkles: Added delete photo support in SPA

--- SPA/src/app/_services/members.service.ts
+++ SPA/src/app/_services/members.service.ts
@@ -9,18 +9,41 @@ import { Observable, map, of } from 'rxjs';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: IMember[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getMembers() {
-    return this.http.get<IMember[]>(this.baseUrl + "users");
+  getMembers(): Observable<IMember[]> {
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<IMember[]>(this.baseUrl + "users").pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      }),
+    );
   }
 
-  getMember(username: string) {
+  getMember(username: string): Observable<IMember> {
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
     return this.http.get<IMember>(this.baseUrl + "users/" + username);
+  }
+
+  updateMember(member: IMember): Observable<void> {
+    return this.http.put(this.baseUrl + "users", member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member};
+      }),
+    );
   }
 
   setMainPhoto(photoId: number): Observable<Object> {
     return this.http.put(this.baseUrl + "users/photo/" + photoId, {});
+  }
+
+  deletePhoto(photoId: number): Observable<Object> {
+    console.log("photoId de nuevo: " + photoId);
+    return this.http.delete(this.baseUrl + "users/photo/" + photoId, {});
   }
 }
